f2f60ca92a7246ea1ba91538b9d2363835dcbf32
rogertad <50554538+rogertad@users.noreply.github.com>
2023-07-18 10:27:27 -0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,156 +1,71 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
 const char* ssid = "chess2g";            // fill in here your router or wifi SSID
 const char* password = "gambitodadama";  // fill in here your router or wifi password
 
+ESP8266WebServer server(80);
+#define RELAY 0  // relay connected to  GPIO0
+int porta = LED_BUILTIN;
+boolean flag = false;
 
-//IPAddress ip(192, 168, 0, 111);      // preencha aqui o endereço IP fixo desejado
-//IPAddress gateway(192, 168, 0, 1);   // preencha aqui o endereço IP do gateway
-//IPAddress subnet(255, 255, 255, 0);  // preencha aqui a máscara de sub-rede
-//IPAddress dns(8, 8, 8, 8);           // preencha aqui o endereço IP do servidor DNS
-
-WiFiServer server(80);
-int PORTA = 0;  // PORTA connected to  GPIO0
-
-//int porta =PORTA;
-
-//int PORTA = 2;
-
-// Variable to store the HTTP request
-String header;
-// Auxiliar variables to store the current output state
-String outputState = "off";
-// Assign output variables to GPIO pins
-
-// Current time
-unsigned long currentTime = millis();
-// Previous time
-unsigned long previousTime = 0;
-// Define timeout time in milliseconds (example: 2000ms = 2s)
-const long timeoutTime = 2000;
-
+int contador = 0;
 
 void setup() {
   Serial.begin(115200);
-  // Initialize the output variables as outputs
 
-  //WiFi.config(ip, gateway, subnet, dns);
+  delay(1000);
 
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
+  pinMode(RELAY, OUTPUT);
+  pinMode(porta, OUTPUT);
+  digitalWrite(RELAY, HIGH);
+  digitalWrite(porta, HIGH);
 
+  delay(1000);
 
-  // Set outputs to LOW
-  //digitalWrite(PORTA, LOW);
-
-  // Connect to Wi-Fi network with SSID and password
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
   WiFi.begin(ssid, password);
+
+
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+    delay(1000);
   }
-  // Print local IP address and start web server
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+
+  server.on("/", handleRoot);
+  //server.on("/getparam", handleParam);
+
   server.begin();
+
+  delay(333);
 }
+
+
+void handleRoot() {
+  flag = !flag;
+  contador++;
+
+  server.stop();
+  server.close();
+  delay(555);
+
+  digitalWrite(RELAY, flag);
+  digitalWrite(porta, flag);
+
+  delay(555);
+
+  server.on("/", handleRoot);
+  server.begin();
+  server.send(200, "text/html", "<h1>Ola, ESP01! contador = " + String(contador) + " </h1>");
+}
+
+
+
+
 void loop() {
-  WiFiClient client = server.available();  // Listen for incoming clients
-  
-  
-  if (client) {                            // If a new client connects,
-  
-    Serial.println("New Client.");         // print a message out in the serial port
-    String currentLine = "";               // make a String to hold incoming data from the client
-    currentTime = millis();
-    previousTime = currentTime;
-  
-  
-    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
-      currentTime = millis();
-      if (client.available()) {  // if there's bytes to read from the client,
-        char c = client.read();  // read a byte, then
-        Serial.write(c);         // print it out the serial monitor
-        header += c;
-        if (c == '\n') {  // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
 
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /on") >= 0) {
-              Serial.println("light on");
-              outputState = "on";
-              digitalWrite(PORTA, HIGH);
-              digitalWrite(1, HIGH);
-              digitalWrite(2, HIGH);
-              digitalWrite(3, HIGH);
-              //delay(1000);
+  //flag = !flag;
 
-            } else if (header.indexOf("GET /off") >= 0) {
-              Serial.println("light off");
-              outputState = "off";
-              digitalWrite(PORTA, LOW);
-              digitalWrite(1, LOW);
-              digitalWrite(2, LOW);
-              digitalWrite(3, LOW);
-              //delay(1000);
-            }
-
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #77878A;}</style></head>");
-
-            // Web Page Heading
-            client.println("<body><h1>ESP8266 Web Server</h1>");
-
-            // Display current state, and ON/OFF buttons for GPIO 5
-            client.println("<p>State " + outputState + "</p>");
-            // If the output5State is off, it displays the ON button
-            if (outputState == "off") {
-              client.println("<p><a href=\"/on\"><button class=\"button button2\">TURN ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/off\"><button class=\"button\">TURN OFF</button></a></p>");
-            }
-
-            client.println("</body></html>");
-
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-
-          } else {  // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-      }
-    }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
+  //digitalWrite(RELAY, flag);
+  //digitalWrite(porta, flag);
+  //delay(1000);
+  server.handleClient();
 }
